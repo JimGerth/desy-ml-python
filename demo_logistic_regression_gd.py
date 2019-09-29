0626ad9e3c5631d4d326e07664fc7292b871fe84
JimGerth <jim.frederic.gerth@gmail.com>
2019-09-29 15:59:35 +0200

    achieving 80% accuracy with logistic regression from sklearn and the self programmed one. cost array isnt working yet tho

--- demo_logistic_regression_gd.py
+++ demo_logistic_regression_gd.py
@@ -3,30 +3,32 @@ import numpy as np
 
 class LogisticRegressionGD(object):
 
-    def __init__(self, eta=0.05, n_iter=100, random_state=1):
-        self.eta = eta
-        self.n_iter = n_iter
+    def __init__(self, learning_rate=0.05, epochs=100, seed=1):
+        self.learning_rate = learning_rate
+        self.epochs = epochs
+        self.seed = seed
 
-    def net_input(self, X):
-        return np.dot(X, self.w_[1:]) + self.w_[0]
+    def weighted_sum(self, X):
+        return X.dot(self.weights) + self.bias
 
     def activation(self, z):
         return 1.0 / (1.0 + np.exp(-np.clip(z, -250, 250)))
 
     def fit(self, X, y):
-        rgen = np.random.RandomState(self.random_state)
-        self.w_ = rgen.normal(loc=0.0, scale=0.01, size=1 + X.shape[1])
-        self.cost_ = []
+        rgen = np.random.RandomState(self.seed)
+        self.weights = rgen.normal(loc=0.0, scale=0.01, size=(X.shape[1], 1))
+        self.bias = rgen.normal(loc=0.0, scale=0.01)
+        self.cost = []
 
-        for _ in range(self.n_iter):
-            net_input = self.net_input(X)
-            output = self.activation(net_input)
+        for _ in range(self.epochs):
+            weighted_sum = self.weighted_sum(X)
+            output = self.activation(weighted_sum)
             errors = (y - output)
-            self.w_[1:] += self.eta * X.T.dot(errors)
-            self.w_[0] += self.eta * errors.sum()
-            cost = -y.dot(np.log(output)) - (1 - y).dot(np.log(1 - output))
-            self.cost_.append(cost)
+            self.weights += self.learning_rate * X.T.dot(y - self.activation(self.weighted_sum(X)))
+            self.bias += self.learning_rate * errors.sum()
+            #cost = -y.dot(np.log(output)) - (1 - y).dot(np.log(1 - output))
+            #self.cost.append(cost)
         return self
 
     def predict(self, X):
-        return np.where(self.activation(self.net_input(X)) >= 0.5, 1, 0)
+        return np.where(self.activation(self.weighted_sum(X)) >= 0.5, 1, 0)
